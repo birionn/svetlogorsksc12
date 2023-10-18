eaa20017614754a7a3e6f33a6722afc6e6db4bfd
birionn <147654494+birionn@users.noreply.github.com>
2023-10-18 13:28:51 +0300

    to React

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import styles from './Card.module.css'
+
+import image from '/bag.jpg'
+
+
+const Card = ({ data }) => {
+    const [isActivate, setIsActivate] = useState(false)
+  return (
+    <div className={styles.cardWrapper}>
+        <div className={styles.card} onClick={()=>setIsActivate(!isActivate)}>
+            <div className={`${styles.cardSide} ${styles.front} ${isActivate ? styles.activate : ''}`}>
+                <img className={styles.cardImg} src={data.img_path}/>
+                <h3 className={styles.cardTitle}>
+                    {data.title}
+                </h3>
+            </div>
+
+            <div className={`${styles.cardSide} ${styles.back} ${isActivate ? styles.activate : ''}`}>
+                <p>
+                    {data.desc}
+                </p>
+                <button className={styles.cardMoreInfo}>
+                    <Link to={`${data.id}`}>More</Link>
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,46 @@
+import styles from './MainPage.module.css'
+import videoBG from '/mainVideo.mp4'
+
+import Footer from '../../components/Footer/Footer'
+import Card from '../../components/Card/Card'
+
+import allData from '../../data/data.json'
+
+const MainPage = () => {
+  return (
+    <>
+        <header className={styles.header}>
+            <video loop autoPlay muted className={styles.headerVideo}>
+                <source src={videoBG} type="video/mp4"/>
+            </video>
+            <h1 className={styles.headerTitle}> СВЕТЛАГОРСК </h1>
+            <p className={styles.headerSubtitle}>Горад з багатай гісторыяй </p>
+        </header>
+
+        <main className={styles.main}>
+            <div className={styles.container}>
+                <h2 className={styles.mainHeader}>
+                Памятныя мясціны нашага горада
+                </h2>
+
+                <div className={styles.mainCardsWrapper}>
+                    {
+                        allData.slice(0, 3).map(data => <Card data={data} key={data.id}/> )
+                    }
+                </div>
+
+                <h2 className={styles.mainHeader}>
+                    Дарога памяці - дарога славы
+                </h2>
+                <div className={styles.mainCardsWrapper}>
+                    {
+                        allData.slice(3).map(data => <Card data={data} key={data.id}/> )
+                    }
+                </div>
+            </div>
+        </main>
+    </>
+  )
+}
+
+export default MainPage
